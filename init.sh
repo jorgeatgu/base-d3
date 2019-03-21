ae709340dd63dc4b5daa29de40e2a3d2547be8d0
Jorge Aznar <jorgeatgu@gmail.com>
2019-03-21 13:09:04 +0100

    reorder perfomance

--- init.sh
+++ init.sh
@@ -2,16 +2,8 @@
 
 function initd3 {
     echo -n "\e[94m\e[1mVamos a lanzar base-d3 ¿ya has creado una carpeta? (s/n): "
-    read -r answer
-        if echo "$answer" | grep -iq "^s" ;
-    then
-        echo "Perfecto, vamos a comenzar con Base"
-        initConCarpeta;
-    else
-        echo -n "Elige el nombre de tu proyecto: "
-        read -r var_name &&
-        mkdir "$var_name" &&
-        cd "$var_name" &&
+
+    function launch {
         mkdir css src js img csv &&
         curl -O "https://raw.githubusercontent.com/jorgeatgu/base-d3/master/{.stylelintrc,.gitignore,.stylelintignore,.prettierrc.json,package.json,gulpfile.js,index.html,d3.min.js,rollup.config.js,index.js}" &&
         cd src &&
@@ -30,5 +22,18 @@ function initd3 {
         echo -e '\e[94m\e[1mEl script ha terminado. Es hora de picar código! \U0001f913\n' &&
         afplay /System/Library/Sounds/Submarine.aiff &&
         say El script ha terminado. Es hora de picar código!
+    }
+
+    read -r answer
+        if echo "$answer" | grep -iq "^s" ;
+    then
+        echo "Perfecto, vamos a comenzar con Base"
+        launch;
+    else
+        echo -n "Elige el nombre de tu proyecto: "
+        read -r var_name &&
+        mkdir "$var_name" &&
+        cd "$var_name" &&
+        launch
     fi
 }
